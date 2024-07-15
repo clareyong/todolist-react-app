4a5fed1d1fe9706f4293701e024be2de3684f2ca
Kevin Winata <kevin.winata@shopee.com>
2024-07-15 22:09:38 +0800

    add todos

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function TodoItem({ value, onChange }) {
 
 function TodoList({ items }) {
   const todoItems = ["shower", "eat", "sleep"];
+  // TODO: Add form how to add to the todolist
+  // TODO: allow rearranging list
+  // TODO: checklisted item should stay at the bottom and will be grayed out
+  // TODO: allow delete item
+  // TODO: add flag
   return (
     <div className="todo-list">
       {todoItems.map(item => (<TodoItem value={item} />))}
