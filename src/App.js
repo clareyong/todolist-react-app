49ab52b7795475f31d5744c7e42ac34ae70eb872
Clare Yong <claremagdelene@gmail.com>
2024-07-16 10:34:39 +0800

    add function to add items

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
+import { useState } from 'react';
+
 function TodoItem({ value, onChange }) {
+  function completedItem() {
+    const [itemComplete, setItemComplete] = useState(Array())
+  
+  
+  }
   return (
     <div className="todo-item">
-      <input type="checkbox" name={value} onchange={onChange}></input>
+      <input type="checkbox" name={value} onChange={onChange}></input>
       <label for={value}>{value}</label>
     </div>
   );
 }
 
-function TodoList({ items }) {
-  const todoItems = ["shower", "eat", "sleep"];
-  // TODO: Add form how to add to the todolist
+function TodoList() {
+  const [todoItems, setTodoItems] = useState(Array());
+  function addItem(formData) {
+    const item = formData.get("item");
+    setTodoItems([...todoItems, item]);
+  }
+  // TODO: checklisted item will be grayed out
+  // TODO: checklisted item should stay at the bottom
   // TODO: allow rearranging list
-  // TODO: checklisted item should stay at the bottom and will be grayed out
   // TODO: allow delete item
   // TODO: add flag
   return (
+    <>
+    <form action={addItem}>
+      <input name="item" />
+      <button type="submit">Add task</button>
+    </form>
     <div className="todo-list">
       {todoItems.map(item => (<TodoItem value={item} />))}
     </div>
+    </>
+   
   );
 }
 
-export default function Game() {
+export default function List() {
+  console.log('test');
   return (
     <>
       <TodoList />
